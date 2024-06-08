25cab4e243830ef518cee5c57cb2f6c6955954bb
morphisjustfun <mariojacobo2011@gmail.com>
2024-06-08 18:37:58 -0500

    📈 update configs

--- search_web/src/services/products.js
+++ search_web/src/services/products.js
@@ -1,6 +1,13 @@
 const baseUrl = 'http://34.128.189.73/api';
 
-export const getProductByProductId = async (productId) => {
-    const response = await fetch(`${baseUrl}/history-product/${productId}`);
-    return await response.json();
+export const getProductByProductId = async (productId, retry = 2) => {
+    try {
+        const response = await fetch(`${baseUrl}/product/${productId}`);
+        return await response.json();
+    } catch (error) {
+        if (retry > 0) {
+            return getProductByProductId(productId, retry - 1);
+        }
+        return null;
+    }
 }
